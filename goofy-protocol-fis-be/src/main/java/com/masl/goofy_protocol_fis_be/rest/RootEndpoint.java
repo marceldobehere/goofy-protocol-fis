a12922a53e6d0866c435469dc62a1bd139af72c4
marceldobehere <marcel.z2006@gmail.com>
2026-08-13 01:56:30 +0200

    UGHHHH HTTP2 WHAZ ARE YOU DOING

--- goofy-protocol-fis-be/src/main/java/com/masl/goofy_protocol_fis_be/rest/RootEndpoint.java
+++ goofy-protocol-fis-be/src/main/java/com/masl/goofy_protocol_fis_be/rest/RootEndpoint.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/")
@@ -22,7 +23,13 @@ public class RootEndpoint {
 
     @GetMapping
     @FisEndpoint(summary = "Redirects to the Frontend URL (should be static) <br>Also appends the `overrideBackendUrl` automatically, so the Frontend talks to the correct Backend")
-    public ResponseEntity<String> index() {
+    public ResponseEntity<String> index(@RequestHeader Map<String, String> headers) {
+        System.out.println("NEW ROOT REQUEST!!!");
+        headers.forEach((key, value) -> {
+            System.out.println("Header: " + key + " = " + value);
+        });
+        System.out.println();
+
         URI redirectUri = UriComponentsBuilder
                 .fromUriString(generalProperties.getFrontendUrl())
                 .queryParam("overrideBackendUrl", generalProperties.getUrl())
