94dd01318a912e5d3da8a39f364985e579e40a70
marceldobehere <marcel.z2006@gmail.com>
2026-08-13 02:04:24 +0200

    broooo 2

--- goofy-protocol-fis-be/src/main/java/com/masl/goofy_protocol_fis_be/rest/RootEndpoint.java
+++ goofy-protocol-fis-be/src/main/java/com/masl/goofy_protocol_fis_be/rest/RootEndpoint.java
@@ -3,6 +3,7 @@ package com.masl.goofy_protocol_fis_be.rest;
 import com.masl.goofy_protocol_fis_be.exception.base.swagger.FisEndpoint;
 import com.masl.goofy_protocol_fis_be.properties.GeneralProperties;
 import io.swagger.v3.oas.annotations.Hidden;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,8 +23,8 @@ public class RootEndpoint {
 
     @GetMapping
     @FisEndpoint(summary = "Redirects to the Frontend URL (should be static) <br>Also appends the `overrideBackendUrl` automatically, so the Frontend talks to the correct Backend")
-    public ResponseEntity<String> index(@RequestHeader(name = "Host", required = false) String host1, @RequestHeader(name = "host", required = false) String host2) {
-        System.out.println("NEW ROOT REQUEST for: " + host1 + " - " + host2);
+    public ResponseEntity<String> index(@RequestHeader(name = "Host", required = false) String host, HttpServletRequest request) {
+        System.out.println("NEW ROOT REQUEST for: " + host + " - " + request.getServerName() + ":" + request.getServerPort() + " - " + request.getRequestURI());
 
         URI redirectUri = UriComponentsBuilder
                 .fromUriString(generalProperties.getFrontendUrl())
